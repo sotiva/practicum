3cfd5fa51bfb15d3e8bd1ba7e89b3e6b6c3ee6ed
sotiva <sotiva@yandex.ru>
2022-07-30 10:01:06 +0500

    изменили файл ./src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+import autorisation from '../src/pages/autorisation';
 import edit_profile from '../src/pages/edit_profile';
-//import style from '@parcel/transformer-sass';
-
-//import change_password from '../src/pages/change_password';
+import change_avatar from '../src/pages/change_avatar';
+import profile from '../src/pages/profile';
+import change_password from '../src/pages/change_password';
+import registration from '../src/pages/registration';
+import page404 from '../src/pages/404';
+import page500 from '../src/pages/500';
 
 const main = document.querySelector("#root");
 
 const routes = {
+    "/autorisation": autorisation,
     "/edit_profile": edit_profile,
-    '/': edit_profile,
+    "/change_password":change_password,
+    "/change_avatar":change_avatar,
+    "/profile": profile,
+    "/registration": registration,
+    "/page404": page404,
+    "/page500": page500,
+    '/': registration,
 };
 
 
